c029216cb3455abb5b247fff3c5ae04f347a5fb1
Mavlon4658 <mavlonbe4658>
2024-10-24 17:53:48 +0500

    home is ready

--- js/main.js
+++ js/main.js
@@ -9,3 +9,43 @@ burger_btn.addEventListener('click', () => {
 nav_close.addEventListener('click', () => {
   nav.classList.remove('active');
 })
+
+let homeSwp = new Swiper('.home .swiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.home .swp-pagination',
+    clickable: true,
+  }
+})
+
+let homeList = document.querySelectorAll('.home ul li');
+
+if (homeList.length) {
+  homeList.forEach((el, elID) => {
+    el.onclick = () => {
+      homeSwp.slideTo(elID);
+      homeList.forEach(i => {
+        if (i == el) {
+          i.classList.add('active');
+        } else {
+          i.classList.remove('active');
+        }
+      })
+    }
+  })
+
+  homeSwp.on('slideChange', function (e) {
+    homeList.forEach((i, idx) => {
+      if (idx == homeSwp.realIndex) {
+        i.classList.add('active');
+      } else {
+        i.classList.remove('active');
+      }
+    })
+  });
+}
