3ab1725ea2209cee25df74bb7b7637fbfc794fa5
Mavlon4658 <mavlonbe4658>
2024-11-04 14:14:06 +0500

    added search block

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
+const bodyHidden = () => {
+  document.querySelector('body').style.overflow = 'hidden';
+};
+
+const bodyVisible = () => {
+  document.querySelector('body').style.overflow = 'visible';
+}
+
 let burger_btn = document.querySelector('.burger_btn');
+let search_btn = document.querySelector('.header_btns, .search-icon');
 let nav_close = document.querySelector('.nav_close');
 let nav = document.querySelector('.nav');
+let searchBtn = document.querySelector('.search_btn button');
+let navLink = document.querySelector('.nav .nav_link');
+
+searchBtn.onclick = () => {
+  navLink.classList.add('active');
+}
 
 burger_btn.addEventListener('click', () => {
   nav.classList.add('active');
+  bodyHidden();
+})
+
+search_btn.addEventListener('click', () => {
+  nav.classList.add('active');
+  bodyHidden();
 })
 
 nav_close.addEventListener('click', () => {
   nav.classList.remove('active');
+  nav.classList.add('end-active');
+  setTimeout(() => {
+    nav.classList.remove('end-active');
+    bodyVisible();
+  }, 400);
 })
 
 let homeSwp = new Swiper('.home .swiper', {
@@ -57,7 +83,8 @@ const historySwpRight = new Swiper('.history__swp_right .swiper', {
   loop: true,
   mousewheel: false,
   navigation: {
-    nextEl: '.history__swp_right .next-btn'
+    nextEl: '.history__swp_right .next-btn',
+    prevEl: '.history__swp_right .prev-btn',
   },
   breakpoints: {
     1300: {
